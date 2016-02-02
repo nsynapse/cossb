64245aa94ea8c2847e8c88593346581f6ca24e7b
bhhwang <bhhwang@nsynapse.com>
2016-02-02 21:29:34 +0900

    add empty file for secuity implementation

---
+++ include/secure/decrypt.hpp
@@ -0,0 +1,18 @@
+
+#ifndef _COSSB_DECRYPT_HPP_
+#define _COSSB_DECRYPT_HPP_
+
+namespace cossb {
+namespace secure {
+
+class decrypt
+{
+public:
+	decrypt();
+	virtual ~decrypt();
+};
+
+} /* namespace secure */
+} /* namespace cossb */
+
+#endif /* _COSSB_DECRYPT_HPP_ */
