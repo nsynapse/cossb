ffe8523828e9d0cf89334cc3e06d2eb02dcfc78f
bhhwang <bhhwang@nsynapse.com>
2016-06-18 11:39:17 +0900

    apply color label (error log only)

--- include/base/log.cpp
+++ include/base/log.cpp
@@ -5,8 +5,13 @@
 #include <util/format.h>
 #include <util/localtime.hpp>
 
+
+
 namespace cossb {
 namespace log {
+
+#define _COLOR_ERROR_	"\033[1;31m"
+#define _COLOR_END_		"\033[0m"
 
 console::console(){ }
 console::console(interface::ilog* log):_logger(log) { }
@@ -28,7 +33,7 @@ void console::log(const log::loglevel& level, const char* logstr)
 		case	loglevel::INFO:		cout << _time.current_formatted() << "[INFO]\t"		<< logstr << endl;	break;
 		case	loglevel::NOTICE: 	cout << _time.current_formatted() << "[NOTICE]\t"	<< logstr << endl;	break;
 		case	loglevel::WARN: 	cout << _time.current_formatted() << "[WARN]\t"		<< logstr << endl;	break;
-		case	loglevel::ERROR: 	cout << _time.current_formatted() << "[ERROR]\t"	<< logstr << endl;	break;
+		case	loglevel::ERROR: 	cout << _COLOR_ERROR_ << _time.current_formatted() << "[ERROR]\t"	<< logstr << _COLOR_END_ << endl;	break;
 		case	loglevel::CRITICAL: cout << _time.current_formatted() << "[CRITICAL]\t"	<< logstr << endl;	break;
 		case	loglevel::ALERT: 	cout << _time.current_formatted() << "[ALERT]\t"	<< logstr << endl;	break;
 		case	loglevel::EMERG: 	cout << _time.current_formatted() << "[EMERG]\t"	<< logstr << endl;	break;
@@ -49,7 +54,7 @@ void console::log(const log::loglevel& level, string logstr)
 		case	loglevel::INFO:		cout << _time.current_formatted() << "[INFO]\t"		<< logstr << endl;	break;
 		case	loglevel::NOTICE: 	cout << _time.current_formatted() << "[NOTICE]\t"	<< logstr << endl;	break;
 		case	loglevel::WARN: 	cout << _time.current_formatted() << "[WARN]\t"		<< logstr << endl;	break;
-		case	loglevel::ERROR: 	cout << _time.current_formatted() << "[ERROR]\t"	<< logstr << endl;	break;
+		case	loglevel::ERROR: 	cout << _COLOR_ERROR_ << _time.current_formatted() << "[ERROR]\t"	<< logstr << _COLOR_END_ << endl;	break;
 		case	loglevel::CRITICAL: cout << _time.current_formatted() << "[CRITICAL]\t"	<< logstr << endl;	break;
 		case	loglevel::ALERT: 	cout << _time.current_formatted() << "[ALERT]\t"	<< logstr << endl;	break;
 		case	loglevel::EMERG: 	cout << _time.current_formatted() << "[EMERG]\t"	<< logstr << endl;	break;
