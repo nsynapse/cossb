771eed0cba5ba72f77d4ee4c95da8e3bb98644d2
elecun <elecun2@gmail.com>
2016-12-03 12:13:01 +0900

    update

--- util/websocket/broadcaster.cpp
+++ util/websocket/broadcaster.cpp
@@ -29,43 +29,45 @@ int main(int argc, char* argv[])
 	//server.config.address = address;
 	cout << server.config.address << " - Waiting for connection on port " << port << endl;
 
-	auto& sensor_epmap = server.endpoint["^/sensor/?$"];
-	auto& command_epmap = server.endpoint["^/command/?$"];
+	auto& sensor_epmap = server.endpoint["^/sensor?$"];
+	auto& command_epmap = server.endpoint["^/command?$"];
 
 	sensor_epmap.onmessage=[&server](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::Message> message) {
 		auto message_str = message->string();
-		cout << "[" << connection.get() << "]" << "\tMessage received(/sensor/) : " << message_str << endl;
+		cout << "[" << connection.get() << "]" << "\tMessage received : " << message_str << endl;
 
 		for(auto a_connection: server.get_connections()) {
 			auto send_stream = make_shared<WsServer::SendStream>();
+			cout << a_connection.get()->path << " - " << connection.get()->path << endl;
 			if(a_connection.get()->path==connection.get()->path && a_connection!=connection){
 				*send_stream << message_str;
 				//server.send is an asynchronous function
 				server.send(a_connection, send_stream);
-				cout << "---Broadcast data to " << connection.get() << endl;
+				cout << "sensor Broadcast data to " << connection.get() << endl;
 			}
 		}
 	};
 
-	sensor_epmap.onopen=[](shared_ptr<WsServer::Connection> connection) { cout << "* New connection(sensor) : " << connection.get() << endl; };
-	sensor_epmap.onclose=[](shared_ptr<WsServer::Connection> connection, int status, const string& reason) { cout << "* Close connection : " << connection.get() << " with status code " << status << endl; };
+	sensor_epmap.onopen=[&server](shared_ptr<WsServer::Connection> connection) { cout << "* New connection : " << "[" << server.get_connections().size() << "] " << connection.get() << endl; };
+	sensor_epmap.onclose=[&server](shared_ptr<WsServer::Connection> connection, int status, const string& reason) { cout << "* Close connection : " << "[" << server.get_connections().size() << "] " << connection.get() << " with status code " << status << endl; };
 	sensor_epmap.onerror=[](shared_ptr<WsServer::Connection> connection, const boost::system::error_code& ec) { cout << "Server: Error in connection " << connection.get() << ". " << "Error: " << ec << ", error message: " << ec.message() << endl; };
 
-	command_epmap.onopen=[](shared_ptr<WsServer::Connection> connection) { cout << "* New connection(command) : " << connection.get() << endl; };
-	command_epmap.onclose=[](shared_ptr<WsServer::Connection> connection, int status, const string& reason) { cout << "* Close connection : " << connection.get() << " with status code " << status << endl; };
+	command_epmap.onopen=[&server](shared_ptr<WsServer::Connection> connection) { cout << "* New connection : " << "[" << server.get_connections().size() << "] " << connection.get() << endl; };
+	command_epmap.onclose=[&server](shared_ptr<WsServer::Connection> connection, int status, const string& reason) { cout << "* Close connection : " << "[" << server.get_connections().size() << "] " << connection.get() << " with status code " << status << endl; };
 	command_epmap.onerror=[](shared_ptr<WsServer::Connection> connection, const boost::system::error_code& ec) { cout << "Server: Error in connection " << connection.get() << ". " << "Error: " << ec << ", error message: " << ec.message() << endl; };
 
 	command_epmap.onmessage=[&server](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::Message> message) {
 			auto message_str = message->string();
-			cout << "[" << connection.get() << "]" << "\tMessage received(/command/) : " << message_str << endl;
+			cout << "[" << connection.get() << "]" << "\tMessage received : " << message_str << endl;
 
 			for(auto a_connection: server.get_connections()) {
 				auto send_stream = make_shared<WsServer::SendStream>();
 				if(a_connection.get()->path==connection.get()->path && a_connection!=connection){
 					*send_stream << message_str;
+					cout << a_connection.get()->path << " / " << a_connection << endl;
 					//server.send is an asynchronous function
 					server.send(a_connection, send_stream);
-					cout << "Broadcast data to " << connection.get() << endl;
+					cout << "command Broadcast data to " << connection.get() << endl;
 				}
 			}
 		};
