d395c4754b3a4cd13b880fa67a78c3aff50310fe
elecun <elecun2@gmail.com>
2018-01-21 21:54:03 +0900

    .

--- components/app_timbo/app_timbo_func.cpp
+++ components/app_timbo/app_timbo_func.cpp
@@ -14,23 +14,17 @@ using namespace std;
 #define HEAD	0x55
 #define END		0xaa
 #define SET		0xf0
-#define TRAJ		0x0d
+#define TRAJ	0x0d
 #define RECORD	0x02
-#define PLAY		0x03
-#define STOP		0x04
-#define PING		0x24
+#define PLAY	0x03
+#define STOP	0x04
+#define PING	0x24
 #define TRAJECTORY_DUMP	0x37
 #define TRAJECTORY_PLAY	0x2e
 
 void app_timbo::key_id_setting(int value){
 	unsigned char frame[] = {HEAD, 0x07, SET, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, END};
 	memcpy(frame+3, _group_id, sizeof(_group_id));
-
-	//print
-	for(int i=0;i<(int)sizeof(frame);i++)
-		cout << (int)frame[i];
-	cout << endl;
-
 	cossb::message _msg(this, base::msg_type::DATA);
 	vector<unsigned char> data(frame, frame+sizeof(frame));
 	cossb_broker->publish("app_timbo_command", _msg);
@@ -54,7 +48,7 @@ void app_timbo::timbo_record(){
 	vector<unsigned char> data(frame, frame+sizeof(frame));
 	_msg.pack(data);
 	cossb_broker->publish("timbo_write", _msg);
-	cossb_log->log(log::loglevel::INFO, fmt::format("Record : Publish to Nanopi : {} bytes", data.size()));
+	cossb_log->log(log::loglevel::INFO, "(APP_TIMBO) publish : 'RECORD' command");
 }
 
 void app_timbo::timbo_play(){
@@ -63,7 +57,7 @@ void app_timbo::timbo_play(){
 	vector<unsigned char> data(frame, frame+sizeof(frame));
 	_msg.pack(data);
 	cossb_broker->publish("timbo_write", _msg);
-	cossb_log->log(log::loglevel::INFO, fmt::format("Play : Publish to Nanopi : {} bytes", data.size()));
+	cossb_log->log(log::loglevel::INFO, "(APP_TIMBO) publish : 'PLAY' command");
 }
 
 
@@ -73,7 +67,7 @@ void app_timbo::timbo_stop(){
 	vector<unsigned char> data(frame, frame+sizeof(frame));
 	_msg.pack(data);
 	cossb_broker->publish("timbo_write", _msg);
-	cossb_log->log(log::loglevel::INFO, fmt::format("Stop : Publish to Nanopi : {} bytes", data.size()));
+	cossb_log->log(log::loglevel::INFO, "(APP_TIMBO) publish : 'STOP' command");
 }
 
 void app_timbo::timbo_ping(){
@@ -82,7 +76,7 @@ void app_timbo::timbo_ping(){
 	vector<unsigned char> data(frame, frame+sizeof(frame));
 	_msg.pack(data);
 	cossb_broker->publish("timbo_write", _msg);
-	cossb_log->log(log::loglevel::INFO, fmt::format("Ping : Publish to Nanopi : {} bytes", data.size()));
+	cossb_log->log(log::loglevel::INFO, "(APP_TIMBO) publish : 'PING' command");
 }
 
 void app_timbo::timbo_trajectory_play(int page, int module){
@@ -103,14 +97,16 @@ void app_timbo::timbo_trajectory_play(int page, int module){
 		cossb_log->log(log::loglevel::INFO, fmt::format("Loaded Trajectory file : page{}_{}.trj ({}bytes) ",page, module, trajectory.size()));
 	}
 	file.close();
-
 	cossb_log->log(log::loglevel::INFO, "Trajectory Playing...");
 
-	unsigned char start[] = {HEAD, 0x06, 0x0f, 0xc, 0x00, 0x55, 0x7f, 0x01, END}; //record start
+
+	unsigned char start[] = {HEAD, 0x06, 0x0f, 0x0c, 0x0, 0x55, 0x7f, 0x01, END}; //record start
 	vector<unsigned char> sdata(start, start+sizeof(start)/sizeof(start[0]));
 	cossb::message smsg(this, base::msg_type::DATA);
 	smsg.pack(sdata);
 	cossb_broker->publish("timbo_write", smsg);
+	boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+
 
 	//2. send start
 	unsigned char header[] = {HEAD, 0x03, 0x0f, 0x2e, 0x00, END}; //record start
@@ -118,17 +114,18 @@ void app_timbo::timbo_trajectory_play(int page, int module){
 	cossb::message hmsg(this, base::msg_type::DATA);
 	hmsg.pack(data1);
 	cossb_broker->publish("timbo_write", hmsg);
+	boost::this_thread::sleep(boost::posix_time::milliseconds(100));
 
 	//3. send trajectory
-	const int offset = 5;
-	for(int i=0;i<trajectory.size();i+=offset){
-		unsigned char trj[] = {HEAD, 0x04, 0x0f, TRAJ, trajectory[i+2], trajectory[i+3], END};
+	const int offset = 20;
+	for(int i=0;i<trajectory.size();i+=offset) {
+		unsigned char trj[] = {HEAD, 0x05, 0x0f, TRAJ, trajectory[i+2], trajectory[i+3], 0x00, END};
 		cossb::message vmsg(this, base::msg_type::DATA);
 		vector<unsigned char> data2(trj, trj+sizeof(trj));
 		vmsg.pack(data2);
 		cossb_broker->publish("timbo_write", vmsg);
 		cossb_log->log(log::loglevel::INFO, fmt::format("Send Trajectory : {}, {}", trajectory[i+2], trajectory[i+3]));
-		boost::this_thread::sleep(boost::posix_time::milliseconds(20));
+		boost::this_thread::sleep(boost::posix_time::milliseconds(100));
 	}
 
 	//4. end
@@ -139,15 +136,15 @@ void app_timbo::timbo_trajectory_play(int page, int module){
 	cossb_broker->publish("timbo_write", tmsg);
 	cossb_log->log(log::loglevel::INFO, "Trajectory Playing...Done.");
 
+
 }
 
 void app_timbo::timbo_trajectory_dump(int page, int module) {
 	unsigned char frame[] = {HEAD, 0x03, TRAJECTORY_DUMP, 0x00, END};
 	vector<unsigned char> packet(frame, frame+sizeof(frame)/sizeof(frame[0]));
 	std::tuple<int, int, vector<unsigned char>> data = std::make_tuple(page, module, packet);
-
 	cossb::message _msg(this, base::msg_type::REQUEST);
 	_msg.pack(data);
 	cossb_broker->publish("timbo_write", _msg);
-	cossb_log->log(log::loglevel::INFO, fmt::format("Trajectory Dump : Publish to Nanopi : {} bytes", packet.size()));
+	cossb_log->log(log::loglevel::INFO, "(APP_TIMBO) publish : 'TRAJECTORY DUMP' command");
 }
