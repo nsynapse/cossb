fc9b38f9022922bde5e1d0a582c727b5f6ce7d73
byunghunhwang <byunghunhwang@localhost>
2018-01-30 01:44:14 +0900

    update

--- components/app_timbo/app_timbo_func.cpp
+++ components/app_timbo/app_timbo_func.cpp
@@ -148,3 +148,20 @@ void app_timbo::timbo_trajectory_dump(int page, int module) {
 	cossb_broker->publish("timbo_write", _msg);
 	cossb_log->log(log::loglevel::INFO, "(APP_TIMBO) publish : 'TRAJECTORY DUMP' command");
 }
+
+void app_timbo::ebook_sound_play()
+{
+	//1. create message instance
+	cossb::message msg(this, cossb::base::msg_type::REQUEST);
+
+	//2. generate json message
+	nlohmann::json _json_msg;
+	_json_msg["command"] = "sound_play";
+
+	//3. pack the json message
+	msg.pack(_json_msg.dump());
+
+	//4. send to broker
+	cossb_broker->publish("websocket_write_msg",msg);
+	cossb_log->log(log::loglevel::INFO, "Sound Play on Ebook");
+}
