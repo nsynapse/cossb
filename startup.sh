6dc6854edbed482853f9d0037639d9b805e3342e
elecun <elecun@DESKTOP-85PU48M.localdomain>
2018-03-09 06:25:45 +0000

    update

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cp ./bin/cossb /usr/local/bin/
+echo "Copying files..."
+cp -rp ./bin/cossb /usr/local/bin/
+cp -rp ./bin/wsbroadcaster /usr/local/bin/
+cp -rp ./bin/lwan /usr/local/bin/
+cp -rp ./bin/lwan.conf /usr/local/bin/
