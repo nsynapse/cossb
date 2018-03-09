f7dd4f1e894ead2101467c154a36b33cb52871c0
elecun <elecun@DESKTOP-85PU48M.localdomain>
2018-03-09 06:42:13 +0000

    .

--- startup.sh
+++ startup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+echo "Build Cossb for Timbo"
+make timbo
 
 echo "Copying files..."
 cp -rp ./bin/cossb /usr/local/bin/
+cp -rp ./bin/wsclient.comp /usr/local/bin/
+cp -rp ./bin/wsclient.xml /usr/local/bin/
+cp -rp ./bin/app_timbo.comp /usr/local/bin/
+cp -rp ./bin/app_timbo.xml /usr/local/bin/
+cp -rp ./bin/timboprotocol.comp /usr/local/bin/
+cp -rp ./bin/timboprotocol.xml /usr/local/bin/
+cp -rp ./bin/nanopi_timbo.comp /usr/local/bin/
+cp -rp ./bin/nanopi_timbo.xml /usr/local/bin/
+cp -rp ./bin/manifest_timbo.xml /usr/local/bin/
+
 cp -rp ./bin/wsbroadcaster /usr/local/bin/
 cp -rp ./bin/lwan /usr/local/bin/
 cp -rp ./bin/lwan.conf /usr/local/bin/
